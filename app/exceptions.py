7291f257655c0c0f2a6ef8b98c2496514766f2e4
XLopchik13 <nikita.petrov.it@gmail.com>
2024-10-22 00:59:58 +0300

    Pytest

--- app/exceptions.py
+++ app/exceptions.py
@@ -24,7 +24,7 @@ class IncorrectEmailOrPasswordException(BookingException):
 
 class TokenExpireException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "Токен истек"
+    detail = "Срок действия токена истек"
 
 
 class TokenAbsentException(BookingException):
@@ -41,6 +41,31 @@ class UserIsNotPresentException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
 
 
-class RoomCannotBeBooked(BookingException):
+class RoomFullyBooked(BookingException):
     status_code = status.HTTP_409_CONFLICT
     detail = "Не осталось свободных номеров"
+
+
+class RoomCannotBeBooked(BookingException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Не удалось забронировать номер ввиду неизвестной ошибки"
+
+
+class DateFromCannotBeAfterDateTo(BookingException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Дата заезда не может быть позже даты выезда"
+
+
+class CannotBookHotelForLongPeriod(BookingException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Невозможно забронировать отель сроком более месяца"
+
+
+class CannotAddDataToDatabase(BookingException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Не удалось добавить запись"
+
+
+class CannotProcessCSV(BookingException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Не удалось обработать CSV файл"
--- app/tests/integration_tests/test_bookings/test_dao.py
+++ app/tests/integration_tests/test_bookings/test_dao.py
@@ -1,19 +1,35 @@
 from datetime import datetime
 
+import pytest
+
 from app.bookings.dao import BookingDAO
 
 
-async def test_add_and_get_booking():
+@pytest.mark.parametrize("user_id, room_id", [
+    (2, 2),
+    (2, 3),
+    (1, 4),
+    (1, 4),
+])
+async def test_booking_crud(user_id, room_id):
     new_booking = await BookingDAO.add(
-        user_id=2,
-        room_id=2,
+        user_id=user_id,
+        room_id=room_id,
         date_from=datetime.strptime("2023-07-10", "%Y-%m-%d"),
         date_to=datetime.strptime("2023-07-24", "%Y-%m-%d"),
     )
 
-    assert new_booking.user_id == 2
-    assert new_booking.room_id == 2
+    assert new_booking["user_id"] == user_id
+    assert new_booking["room_id"] == room_id
 
-    new_booking = await BookingDAO.find_by_id(new_booking.id)
+    new_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
 
     assert new_booking is not None
+
+    await BookingDAO.delete(
+        id=new_booking["id"],
+        user_id=user_id,
+    )
+
+    deleted_booking = await BookingDAO.find_one_or_none(id=new_booking["id"])
+    assert deleted_booking is None
